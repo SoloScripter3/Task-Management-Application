82f152c4f980510ab10a3ae696cfcf5129f00b81
SoloScripter3 <vardhanv6259@gmail.com>
2025-02-28 13:31:35 +0530

    add config and user model

---
+++ server/src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Database connected successfully");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectDB();
+
+export default connectDB;
