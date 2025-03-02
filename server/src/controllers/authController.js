5c9afc898e5ce7055eadc6f23988be9169aec869
SoloScripter3 <vardhanv6259@gmail.com>
2025-03-02 23:56:22 +0530

    add redis storage to save creds

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,10 @@
 import User from "../models/users.js";
 import { otpGenerator, sendOtp } from "../utils/mailSender.js";
+import redisClient, {
+  setWithExpiry,
+  getData,
+  deleteData,
+} from "../config/redisSetup.js";
 
 export const register = async (req, res) => {
   try {
@@ -11,9 +16,16 @@ export const register = async (req, res) => {
       return res.status(400).json({ message: "user already exist" });
     }
 
+    //hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     //generating otp and sending to the email
     const otp = otpGenerator();
     await sendOtp(email, otp);
+
+    //store the otp and credentials in redis temporarily
+    await setWithExpiry(email, { email, password: hashedPassword, otp }, 300);
 
     return res.status(200).json({ message: "otp sent successfully" });
   } catch (err) {
