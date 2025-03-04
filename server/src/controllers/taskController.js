780669b243f194ad38b91cc72ad0b07f26ab294b
SoloScripter3 <vardhanv6259@gmail.com>
2025-03-04 17:06:12 +0530

    add CRUD routes

---
+++ server/src/controllers/taskController.js
@@ -0,0 +1,109 @@
+import Task from "../models/tasks.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+
+//creating the task function
+export const createTask = async (req, res) => {
+  try {
+    const { title, description, status = "incomplete" } = req.body;
+
+    //accessing the author details from the authMiddleware
+    const author = req.user._id;
+
+    //creating an new task
+    const newTask = new Task({
+      title,
+      description,
+      author,
+      status,
+    });
+
+    //saving the task to the database
+    await newTask.save();
+
+    //sending the response
+    return res.status(201).json({
+      success: true,
+      message: "task created successfully",
+      data: newTask,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "server error" });
+  }
+};
+
+//getting all the tasks
+export const getTasks = async (req, res) => {
+  try {
+    //getting all the tasks from the database
+    const tasks = await Task.find();
+
+    //sending the response
+    return res.status(200).json({
+      success: true,
+      message: "fetched the tasks successfully",
+      data: tasks,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "server error" });
+  }
+};
+
+//updating the tasks
+export const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, status = "incomplete" } = req.body;
+
+    //finding the task by id
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ message: "task not found" });
+    }
+
+    //updating the task
+    task.title = title;
+    task.description = description;
+    task.status = status;
+    task.updatedAt = Date.now();
+
+    //saving the updated task
+    await task.save();
+
+    //sendind the response
+    return res.status(200).json({
+      success: true,
+      message: "task updated successfully",
+      data: task,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "server error" });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    //finding the task by id and delete
+    const task = await Task.findByIdAndDelete(id);
+
+    if (!task) {
+      return res.status(404).json({ message: "task not found" });
+    }
+
+    //sending the response
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "task deleted successfully",
+        data: task,
+      });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "server error" });
+  }
+};
---
+++ server/src/models/tasks.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "incomplete",
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    completedAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
